f3c9ae0a78beebb9e79e18f82277c02da54a7db6
Austin Frick <austin.frick429@gmail.com>
2024-06-26 20:42:50 -0400

    Basic controller code -AF

---
+++ PlayFolioShelf.WebApi/Controllers/MaturityController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace PlayFolioShelfWebApi.Controllers ;
+
+[Route("api[controller]")] 
+[ApiController] 
+public class MaturityController : ControllerBase 
+{
+
+}
